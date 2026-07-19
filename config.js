d81aaf465182d45b79f4824e0b542521f0e5f6c3
chatterjeekisorgopal-tech <chatterjeekisorgopal@gmail.com>
2026-07-19 14:23:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const CONFIG = {
   CENTER_NAME_SHORT: "Paruliya YCTC",
 
   // Paste your Google Apps Script Web App URL here after deployment (Step 2)
-  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzTxTf4ycqkKq-RPDaR3ebU1q_ju6UlqvKTH5-oE73FKXcVY8HfEOkeeq7KFt9RyJoWnw/exec",
+  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzTxTf4ycqkKq-RPDaR3ebU1q_ju6UlqvKTH5-oE73FKXcVY8HfEOkeeq7KFt9RyJoWnw/exec",https://script.google.com/macros/s/AKfycbwSPotGtbnW8DBWp3_8138AlCmj5_Pphm6DEwK9q1FeX-i9qatQfTQbU7ci4eFkI6s-/exec
 
   COURSES: [
     "CITA",
